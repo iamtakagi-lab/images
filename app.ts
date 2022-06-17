f1fd690a533dca869a3f3ec34f4e45a9fb58d997
takagi <iam.takagi.dev@gmail.com>
2022-06-17 17:07:14 +0900

    2 * 1024 * 1024 (?)

--- app.ts
+++ app.ts
@@ -96,7 +96,7 @@ const storage = multer.diskStorage({
 });
 const upload = multer({
   storage,
-  limits: { fileSize: 1 * 2048 * 2048 },
+  limits: { fileSize: 2 * 1024 * 1024 },
   fileFilter: (req, file, cb) => {
     if (
       file.mimetype === "image/png" ||
